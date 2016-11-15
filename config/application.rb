3528ed36ccba551604c17e5e666ddffd05b16404
LilyCole <Lilia.Cole@gmail.com>
2016-11-15 15:19:27 -0800

    adds :put and :delete for middle-ware

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,7 @@ module UserManagerApi
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :options]
+        resource '*', :headers => :any, :methods => [:get, :post, :options, :put, :delete]
       end
     end
 
