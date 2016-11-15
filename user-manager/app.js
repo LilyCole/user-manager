dbb9778f03f7bf636b68906b85680032eadecda7
LilyCole <Lilia.Cole@gmail.com>
2016-11-15 15:13:02 -0800

    adds http get for users

--- user-manager/app.js
+++ user-manager/app.js
@@ -12,6 +12,13 @@ app.config(function($routeProvider) {
     })
 });
 
-app.controller("UsersController", function() {
-
+app.controller("UsersController", function($http) {
+  $http({
+  method: 'GET',
+  url: 'http://localhost:3000/users'
+    }).success(function(response) {
+        $('body').append(response);
+    }).error(function(response) {
+        alert("Error getting users");
+    });
 });
