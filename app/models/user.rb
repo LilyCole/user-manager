1f82fc032568af18e7b92d40f3baff31700a1382
LilyCole <Lilia.Cole@gmail.com>
2016-11-15 13:39:19 -0800

    set auth_token before user#create in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,20 @@
 class User < ApplicationRecord
   has_secure_password
   validates :first_name, :last_name, :email, :username, :password, presence: true
+  before_create :set_auth_token
+
+  private
+
+  def set_auth_token
+      if auth_token.present?
+          return
+      else
+          self.auth_token = generate_auth_token
+      end
+  end
+
+  def generate_auth_token
+      return SecureRandom.uuid.gsub(/\-/, '')
+  end
+  
 end
