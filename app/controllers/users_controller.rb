3ec02f43de1c61357f0323958a2126a30c5cd335
LilyCole <Lilia.Cole@gmail.com>
2016-11-15 11:52:42 -0800

    adds password to user#create params and validates presence in model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -43,7 +43,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:first_name,:last_name,:username,:email)
+    params.require(:user).permit(:first_name,:last_name,:username,:email,:password)
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ApplicationRecord
-  validates :first_name, :last_name, :email, :username, presence: true
+  validates :first_name, :last_name, :email, :username, :password, presence: true
   validates :username, uniqueness: true
 end
