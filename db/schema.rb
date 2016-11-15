c08fd2e79454b4402908214b9382c378427d7c1d
LilyCole <Lilia.Cole@gmail.com>
2016-11-15 13:33:05 -0800

    adds new column auth_token to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161115194112) do
+ActiveRecord::Schema.define(version: 20161115212839) do
 
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20161115194112) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
+    t.string   "auth_token"
   end
 
 end
