21735065f689a9b7e3b5332dcdeb320d60dc1ec4
LilyCole <Lilia.Cole@gmail.com>
2016-11-16 10:56:33 -0800

    removed password validation for update

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
-  validates :first_name, :last_name, :email, :username, :password, presence: true
+  
+  validates :first_name, :last_name, :email, :username, presence: true
+  
   before_create :set_auth_token
 
   private
