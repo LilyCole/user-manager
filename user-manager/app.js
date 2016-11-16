b738f4b293ae1e0b3e590a543f4df152ecef08ca
LilyCole <Lilia.Cole@gmail.com>
2016-11-15 16:05:55 -0800

    enables user to be created and displayed

--- user-manager/app.js
+++ user-manager/app.js
@@ -7,18 +7,44 @@ app.config(function($routeProvider) {
       controller: "UsersController",
       controllerAs: "usersCtrl"
     })
+    .when("/users/:id/edit", {
+      templateUrl: "templates/edit-user.html",
+      controller: "EditController",
+      controllerAs: "editCtrl"
+    })
     .otherwise({
       redirectTo: "/users"
     })
 });
 
 app.controller("UsersController", function($http) {
+
+  var vm = this;
+
   $http({
   method: 'GET',
   url: 'http://localhost:3000/users'
     }).success(function(response) {
-        $('body').append(response);
+        vm.users = response;
     }).error(function(response) {
         alert("Error getting users");
     });
+
+  vm.submitUser = function(event) {
+    event.preventDefault();
+    $http({
+      method: 'POST',
+      url: 'http://localhost:3000/users',
+      data: {
+          user: vm.user
+        }
+      }).success(function(newUser) {
+          vm.users.push(newUser);
+          vm.user = {};
+          $("#add-user-modal").modal("hide");
+      }).error(function(response) {
+          alert("Error saving user");
+      });
+  }
+  
 });
